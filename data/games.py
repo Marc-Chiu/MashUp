7d6c8770a9132e39f5bf04347cb9fe3846a27e9a
hahyoung-kim <hyk323@nyu.edu>
2023-10-18 14:17:47 -0400

    added games.py and test

---
+++ data/games.py
@@ -0,0 +1,19 @@
+"""
+games.py: the interface to our game data.
+"""
+NUM_PLAYERS = 'numPlayers'
+TEST_GAME_NAME = 'Wizards Galore'
+
+
+games = {
+    'Dungeons and Dragons': {
+        NUM_PLAYERS: 3,
+    },
+    TEST_GAME_NAME: {
+        NUM_PLAYERS: 5,
+    },
+}
+
+
+def get_games() -> dict:
+    return games
