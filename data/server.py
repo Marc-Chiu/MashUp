69e2ce3d38864d3a8dbc2a1605da3540b1cbf7f1
SwanyeeA <sya260@nyu.edu>
2023-10-23 00:03:05 -0400

    added credential verificaition/routing for customer/resturant

--- data/server.py
+++ data/server.py
@@ -1,3 +1,5 @@
+import Flask, render_template, request, redirect, url_for
+import sqlite3
 class SimpleServer:
     def __init__(self):
         self.users = []  # In-memory user storage
@@ -13,3 +15,32 @@ class SimpleServer:
         self.users.append({'email': email, 'password': password})
         print("User added successfully.")
         return True
+
+
+app = Flask(__name)
+
+# A list to store restaurant data (in practice, you'd use a database)
+restaurants = [
+    {'name': 'Restaurant A', 'description': 'Great food and ambiance'},
+    {'name': 'Restaurant B', 'description': 'Family-friendly dining'},
+]
+
+def check_credentials(email, password):
+    cursor.execute("SELECT * FROM users WHERE email=? AND password=?", (email, password))
+    user = cursor.fetchone()
+    return user is not None
+
+@app.route('/')
+def index():
+    return render_template('index.html', restaurants=restaurants)
+
+@app.route('/customer')
+def customer_page():
+    return render_template('customer.html', restaurants=restaurants)
+
+@app.route('/restaurant')
+def restaurant_page():
+    return render_template('restaurant.html', restaurants=restaurants)
+
+if __name__ == '__main__':
+    app.run(debug=True)
