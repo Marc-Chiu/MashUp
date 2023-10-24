4c0a4b4ce624d860ac1b6ee245dcce285c176dc2
marcchiu <mmc9967@nyu.edu>
2023-10-24 19:05:34 -0400

    added attributes and tests to groups endpoint

--- data/groups.py
+++ data/groups.py
@@ -3,6 +3,8 @@ This module interfaces to our groups data
 """
 
 MIN_Group_NAME_LEN = 2
+MEMBERS = "Members"
+RESTAURANTS = "Restaurants"
 
 
 def get_groups():
@@ -12,13 +14,16 @@ def get_groups():
         - Returns a dictionary of groups keyed on group name (a str).
         - Each user name must be the key for a dictionary.
         - Each group must have a list of memembers
+        - Each group must have a list of liked restaurants
     """
     groups = {
         "Foodies": {
-            'Memeber1': "Marc",
+            MEMBERS: ["Marc"],
+            RESTAURANTS: ["Shack Shake"],
         },
         "Reddy": {
-            'Member2': "Red",
+            MEMBERS: ["Red"],
+            RESTAURANTS: ["Caine's"],
         },
     }
     return groups
--- data/tests/test_groups.py
+++ data/tests/test_groups.py
@@ -4,7 +4,10 @@ def test_get_groups():
      groups = grps.get_groups()
      assert isinstance(groups, dict)
      assert len(groups) > 0
-     for group in groups:
-         assert isinstance(group, str)
-         assert isinstance(groups[group], dict)
+     for key in groups:
+         group = groups[key]
+         assert isinstance(key, str)
+         assert isinstance(group, dict)
+         assert isinstance(group[grps.MEMBERS], list)
+         assert isinstance(group[grps.RESTAURANTS], list)
                     
