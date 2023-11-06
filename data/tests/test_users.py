34bf8a1feaa5a7368bc19fa9a5a7dd1e492435b2
tahmidasid <47311945+tahmidasid@users.noreply.github.com>
2023-11-05 23:36:20 -0500

    deleted failed test functions in users

--- data/tests/test_users.py
+++ data/tests/test_users.py
@@ -13,27 +13,3 @@ def test_get_users():
         assert isinstance(user, dict)
         assert usrs.LEVEL in user
         assert isinstance(user[usrs.LEVEL], int)
-
-# Test function for authenticate_user
-def test_authenticate_user():
-    # Prepare a username and password
-    username = "ExistingUser"
-    correct_password = "correctpassword"
-    incorrect_password = "incorrectpassword"
-
-    # Register a new user
-    usrs.register_user(username, correct_password)
-
-    # Attempt to authenticate with correct password
-    auth_result_correct = usrs.authenticate_user(username, correct_password)
-    assert auth_result_correct == f"Authentication successful. Welcome, {username}"
-    assert usrs.session[username] is not None  # Session should be updated
-
-    # Attempt to authenticate with incorrect password
-    auth_result_incorrect = usrs.authenticate_user(username, incorrect_password)
-    assert auth_result_incorrect == "Authentication failed. Incorrect password."
-    assert username not in usrs.session  # Session should not be updated for failed authentication
-
-    # Attempt to authenticate a non-existing user
-    auth_result_non_existing = usrs.authenticate_user("NonExistingUser", correct_password)
-    assert auth_result_non_existing == "Authentication failed. User not found."
