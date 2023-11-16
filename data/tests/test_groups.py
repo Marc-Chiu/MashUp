a1f1334fb3d4c68d20fdeec6703ba035bd70f320
hahyoung-kim <hyk323@nyu.edu>
2023-11-16 15:07:55 -0500

    fixed variable name in test_groups.py

--- data/tests/test_groups.py
+++ data/tests/test_groups.py
@@ -81,7 +81,7 @@ ADD_NAME = 'New Group'
 
 def test_add_group():
     new_name = grps._get_test_name()
-    ret = grps.add_game(new_name, 4)
+    ret = grps.add_group(new_name, 4)
     assert grps.exists(new_name)
     assert isinstance(ret, bool)
-    grps.del_game(new_name)
+    grps.del_group(new_name)
