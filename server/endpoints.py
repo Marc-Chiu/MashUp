8605e7f458116be6d29acb92575c09ef3a6042cc
hahyoung-kim <hyk323@nyu.edu>
2024-02-05 11:34:30 -0500

    Added flask cors to server

--- server/endpoints.py
+++ server/endpoints.py
@@ -6,6 +6,7 @@ from http import HTTPStatus
 
 from flask import Flask, request
 from flask_restx import Resource, Api, fields
+from flask_cors import CORS
 
 import werkzeug.exceptions as wz
 
@@ -14,6 +15,7 @@ import data.groups as grps
 import data.restaurants as restrnts
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 DELETE = 'delete'
